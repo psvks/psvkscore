082e1cb458b8cfeecfcd2a48171187f4edd6b13f
psvks <113241132+psvks@users.noreply.github.com>
2024-05-28 16:25:45 +0200

    Create installer.lua

---
+++ installer.lua
@@ -0,0 +1,4 @@
+print('Installing psvkscore on your computer.')
+shell.run('mkdir psvkscore')
+shell.run('wget https://raw.githubusercontent.com/psvks/psvkscore/main/psvkscore/state.lua psvkscore/state.lua')
+print('Installed.')
